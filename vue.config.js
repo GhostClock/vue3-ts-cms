ce48a9ce283d059b2011843db411367bb486eea8
GhostClock <zhangyuanlaifen@gmail.com>
2023-07-24 22:15:22 +0800

    feat(vue.config配置修改): 修改部分vue.config.js配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // CLI提供的属性
+  outputDir: './build',
+  transpileDependencies: true,
+  // 配置方式一：增加配置信息 PS:这里@默认配置里面已经有了
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // },
+
+  //  配置方式二：覆盖默认配置
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // },
+
+  //  配置方式三：方式二的链式
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', '@/components')
+  }
 })
