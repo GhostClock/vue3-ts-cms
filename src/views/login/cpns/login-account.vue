50c6438a8d00adab5cc7f6432aa862af938163db
GhostClock <zhangyuanlaifen@gmail.com>
2023-07-30 12:15:48 +0800

    feat(登录页): 帐号登录页的表单验证抽取到单独的配置文件中

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -17,38 +17,12 @@
 
 <script lang="ts" setup>
 import { reactive } from 'vue'
+import { rules } from '../config/account-config'
 
 const account = reactive({
   name: '',
   password: ''
 })
-// 编写表单检验规则
-const rules = {
-  name: [
-    {
-      required: true,
-      message: '用户名是必传内容',
-      trigger: 'blur'
-    },
-    {
-      pattern: /^[a-zA-Z0-9]{5,10}$/,
-      message: '用户名必须是5-10个字母或者数字',
-      trigger: 'blur'
-    }
-  ],
-  password: [
-    {
-      required: true,
-      message: '密码是必传内容',
-      trigger: 'blur'
-    },
-    {
-      pattern: /^[a-zA-Z0-9]{3,}$/,
-      message: '密码必须是3位以上字母或者数字',
-      trigger: 'blur'
-    }
-  ]
-}
 </script>
 
 <style lang="less" scoped>
