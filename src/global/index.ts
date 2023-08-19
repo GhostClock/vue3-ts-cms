661d75b4f1c5414b4a256c243c8b025a66e1b646
GhostClock <zhangyuanlaifen@gmail.com>
2023-08-19 22:25:22 +0800

    fix(刷新页面跳转出错): 解决二级路由刷新页面时错误的问题

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,3 +1,9 @@
+// 不管是全局导入还是局部导入，都要引入样式
+import 'element-plus/dist/index.css'
+// 重置默认css样式
+import 'normalize.css'
+import '@/assets/css/index.less'
+
 import { App } from 'vue'
 import { createPinia } from 'pinia'
 import router from '@/router'
@@ -7,6 +13,8 @@ export function registerApp(app: App<Element>): void {
   // 注册全局的内容
   const store = createPinia()
   app.use(store)
-  app.use(router)
+  // 解决刷新页面跳转到notFond页面的问题
+  // 原因：由于路由守卫是后面回调的，开始已经了错误的匹配，导致路由跳转错误
   setupStore()
+  app.use(router)
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { registerApp } from '@/global'
-// 不管是全局导入还是局部导入，都要引入样式
-import 'element-plus/dist/index.css'
-
-// 重置默认css样式
-import 'normalize.css'
-import '@/assets/css/index.less'
 
 const app = createApp(App)
 registerApp(app)
